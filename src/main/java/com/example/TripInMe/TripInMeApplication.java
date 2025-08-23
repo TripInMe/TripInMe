1a5e1344cf3869587852f3fbe2e719716d1e161e
강동연 <kdy2541@naver.com>
2025-08-23 16:26:34 +0900

    chore: initial commit (TripInMe)

---
+++ src/main/java/com/example/TripInMe/TripInMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TripInMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripInMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripInMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TripInMe/TripInMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TripInMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripInMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
